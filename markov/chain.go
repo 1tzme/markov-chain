c7f2a3c578d91cfe121f807e9344de9312832dd7
1tzme <zakir.itsme@gmail.com>
2025-05-18 22:36:28 +0500

    common dev

---
+++ markov/chain.go
@@ -0,0 +1,67 @@
+package markov
+
+import (
+	"bufio"
+	"io"
+	"math/rand"
+	"strings"
+)
+
+type Prefix []string
+
+func (p Prefix) String() string {
+	return strings.Join(p, " ")
+}
+
+func (p Prefix) Shift(word string) {
+	copy(p, p[1:])
+	p[len(p)-1] = word
+}
+
+type Chain struct {
+	chain map[string][]string
+	prefixLen int
+}
+
+func NewChain(prefixLen int) *Chain {
+	return &Chain {
+		chain: make(map[string][]string),
+		prefixLen: prefixLen,
+	}
+}
+
+func (c *Chain) HasPrefix(p Prefix) bool {
+	_, ok := c.chain[p.String()]
+	return ok
+}
+
+func (c *Chain) Build(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	
+	p := make(Prefix, c.prefixLen)
+	for scanner.Scan() {
+		word := scanner.Text()
+		key := p.String()
+		c.chain[key] = append(c.chain[key], word)
+		p.Shift(word)
+	}
+}
+
+func (c *Chain) Generate(n int, start Prefix) string {
+	words := []string{}
+	p := make(Prefix, c.prefixLen)
+	copy(p, start)
+
+	for i := 0; i < n; i++ {
+		choices := c.chain[p.String()]
+		if len(choices) == 0 {
+			break
+		}
+
+		next := choices[rand.Intn(len(choices))]
+		words = append(words, next)
+		p.Shift(next)
+	}
+	return strings.Join(words, " ")
+}
---
+++ markov/flag.go
@@ -0,0 +1,55 @@
+package markov
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ParseFlags() (int, int, string) {
+	wordLimit := 100
+	prefixLen := 2
+	startPref := ""
+
+	args := os.Args[1:]
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "-w":
+			i++
+			if i >= len(args) {
+				fmt.Println("Missing value for -w")
+				os.Exit(1)
+			}
+			val, err := strconv.Atoi(args[i])
+			if err != nil || val <= 0 || val > 10000 {
+				fmt.Println("Number of words should be between 1 and 10000")
+				os.Exit(1)
+			}
+			wordLimit = val
+		case "-l":
+			i++
+			if i >= len(args) {
+				fmt.Println("Missing value for -l")
+				os.Exit(1)
+			}
+			val, err := strconv.Atoi(args[i])
+			if err != nil || val < 1 || val > 5 {
+				fmt.Println("Prefix len should be between 1 and 5")
+				os.Exit(1)
+			}
+			prefixLen = val
+		case "-p":
+			i++
+			if i >= len(args) {
+				fmt.Println("Missing value for -p")
+				os.Exit(1)
+			}
+			startPref = args[i]
+		default:
+			fmt.Println("Unknown flag: " + args[i])
+			os.Exit(1)
+		}
+	}
+
+	return wordLimit, prefixLen, startPref
+}
